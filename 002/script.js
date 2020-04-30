8484ba26cc3d116d2a752a5e29a5c190fd6c7163
Charles Desiderio <cjdesi@gmail.com>
2020-04-29 20:59:58 -0500

    Refactor 002

--- 002/script.js
+++ 002/script.js
@@ -2,29 +2,16 @@ let tasks = [[8, 0], [2, 0], [5, 0], [3, 0], [20, 0], [12, 0], [7, 0], [12, 0], 
 let totalProgress = 0;
 //13 tasks
 
+let update = (arg) => {
+    document.querySelector(".counter").innerHTML = `${totalProgress}% Complete`
+    document.querySelector(".prog-bar").style.width = `${totalProgress}%`
+    document.getElementById(arg).style.textDecoration = tasks[arg - 1][1] ? "none" : "line-through";
+    document.querySelector(".prog-bar").style.backgroundColor = totalProgress === 100 ? "green" : "red";
+}
+
 let progress = (arg) => {
-    
-    if (!tasks[arg -1][1]) {
-        totalProgress += tasks[arg-1][0];
-        tasks[arg-1][1] = 1;
-        document.querySelector(".counter").innerHTML = `${totalProgress}% Complete`
-        document.querySelector(".prog-bar").style.width = `${totalProgress}%`
-        document.getElementById(arg).style.textDecoration = "line-through"
-        if (totalProgress === 100) {
-            document.querySelector(".prog-bar").style.backgroundColor = "green"
-        } else {
-            document.querySelector(".prog-bar").style.backgroundColor = "red"
-        }
-    } else {
-        totalProgress -= tasks[arg-1][0];
-        tasks[arg-1][1] = 0;
-        document.querySelector(".counter").innerHTML = `${totalProgress}% Complete`
-        document.querySelector(".prog-bar").style.width = `${totalProgress}%`
-        document.getElementById(arg).style.textDecoration = "none"
-        if (totalProgress === 100) {
-            document.querySelector(".prog-bar").style.backgroundColor = "green"
-        } else {
-            document.querySelector(".prog-bar").style.backgroundColor = "red"
-        }
-    }
+
+    totalProgress = tasks[arg-1][1] ? totalProgress - tasks[arg-1][0] : totalProgress + tasks[arg-1][0];
+    tasks[arg-1][1] = !tasks[arg-1][1];
+    update(arg);
 }
